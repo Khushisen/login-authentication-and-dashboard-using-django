44573d554758aad036dcfcf765be9ae7b498adbd
khushisen <khushisen9001@gmail.com>
2024-12-22 23:11:38 +0530

    second commit

--- practice/wscube/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from wscube.models import Service
-
-class ServiceAdmin(admin.ModelAdmin):
-    list_display=('service_icon','service_title','service_des')
-
-admin.site.register(Service,ServiceAdmin)
