ed9ffa2599519180e98ee4fead194fb1f3d1294a
khushisen <khushisen9001@gmail.com>
2024-12-24 14:22:03 +0530

    updated css

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "practice.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates','static'],
+        "DIRS": ['templates',],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
